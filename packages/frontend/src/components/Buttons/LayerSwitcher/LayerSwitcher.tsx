4fc446ab179fcbbcbcaec1675d6d41832b043d92
Johan Trieloff <johan@trieloff.net>
2024-06-16 22:11:55 +0200

    Indicate current state

--- packages/frontend/src/components/Buttons/LayerSwitcher/LayerSwitcher.tsx
+++ packages/frontend/src/components/Buttons/LayerSwitcher/LayerSwitcher.tsx
@@ -14,6 +14,8 @@ const LayerSwitcher: React.FC<LayerSwitcherProps> = ({ onClick, isRiskLayerOpen 
       <button 
         className='layer-switcher small-button' 
         onClick={() => setAreLayerOptionsVisible(!areLayerOptionsVisible)}
+        onMouseEnter={() => setAreLayerOptionsVisible(true)}
+        onMouseLeave={() => setAreLayerOptionsVisible(false)}
       >
         <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M19.7085 1.75047C19.2627 1.5276 18.7381 1.5276 18.2922 1.75047L2.45896 9.66713C1.92256 9.93533 1.58371 10.4836 1.58371 11.0833C1.58371 11.683 1.92256 12.2313 2.45896 12.4995L18.2922 20.4161C18.7381 20.639 19.2627 20.639 19.7085 20.4161L35.5419 12.4995C36.0781 12.2313 36.417 11.683 36.417 11.0833C36.417 10.4836 36.0781 9.93533 35.5419 9.66713L19.7085 1.75047Z" fill="black"/>
@@ -25,11 +27,19 @@ const LayerSwitcher: React.FC<LayerSwitcherProps> = ({ onClick, isRiskLayerOpen 
       {areLayerOptionsVisible && (
             <div className='layer-options small-button align-child-on-line'>
               <div>
-                <img src={process.env.PUBLIC_URL + '/icons/risk.png'} alt='Showing how the risk layer looks like'></img>
+                <img 
+                  src={process.env.PUBLIC_URL + '/icons/risk.png'} 
+                  alt='Showing how the risk layer looks like'
+                  className={isRiskLayerOpen ? 'active' : ''}
+                ></img>
                 <p>Risiko</p>
               </div>
               <div>
-                <img src={process.env.PUBLIC_URL + '/icons/lines.png'} alt='Showing how the line layer looks like'></img>
+                <img 
+                  src={process.env.PUBLIC_URL + '/icons/lines.png'} 
+                  alt='Showing how the line layer looks like'
+                  className={isRiskLayerOpen ? '' : 'active'}
+                ></img>
                 <p>Linien</p>
               </div>
             </div>
