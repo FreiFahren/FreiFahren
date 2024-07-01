f6bbd0fb668cd26222737237ba5bc9bd18793124
David Brandes <brandes.1@campus.tu-berlin.de>
2024-07-01 18:44:51 +0200

    Fix imports and update dependencies

--- packages/telegram_bots/FreiFahren_BE_NLP/main.py
+++ packages/telegram_bots/FreiFahren_BE_NLP/main.py
@@ -11,17 +11,17 @@ from telegram_bots.FreiFahren_BE_NLP.process_message import (
     format_text,
     lines_with_stations,
     load_data,
-    check_for_spam
+    check_for_spam,
 )
 from telegram_bots.FreiFahren_BE_NLP.db_utils import insert_ticket_info
 from telegram_bots.FreiFahren_BE_NLP.app import app
 from telegram_bots.logger import setup_logger
-from telegram_bots. FreiFahren_BE_NLP.bot import nlp_bot, start_bot
+from telegram_bots.FreiFahren_BE_NLP.bot import nlp_bot, start_bot
 import traceback
 import requests
 import sys
 import threading
-from telegram_bots.config import WATCHER_URL, BACKEND_URL
+from telegram_bots.config import WATCHER_URL
 
 
 class TicketInspector:
@@ -30,11 +30,12 @@ class TicketInspector:
         self.station = station
         self.direction = direction
 
+
 def extract_ticket_inspector_info(unformatted_text):
     # Initial guards to avoid unnecessary processing
-    if '?' in unformatted_text or check_for_spam(unformatted_text):
+    if "?" in unformatted_text or check_for_spam(unformatted_text):
         ticket_inspector = TicketInspector(line=None, station=None, direction=None)
-        logger.info('Message is not getting processed')
+        logger.info("Message is not getting processed")
         return ticket_inspector.__dict__
 
     found_line = find_line(unformatted_text, lines_with_stations)
@@ -60,63 +61,67 @@ def extract_ticket_inspector_info(unformatted_text):
         return None
 
 
-stations_dict = load_data('data/stations_list_main.json')
+stations_dict = load_data("data/stations_list_main.json")
 
 
 def process_new_message(timestamp, message):
     info = extract_ticket_inspector_info(message.text)
-    if (type(info) is dict):
+    if type(info) is dict:
         found_items = []
-        if info.get('line'):
-            found_items.append('line')
-        if info.get('station'):
-            found_items.append('station')
-        if info.get('direction'):
-            found_items.append('direction')
-        
+        if info.get("line"):
+            found_items.append("line")
+        if info.get("station"):
+            found_items.append("station")
+        if info.get("direction"):
+            found_items.append("direction")
+
         # Avoid logging the actual data to avoid storing data with which the user could be identified
         if found_items:
-            logger.info('Found Info: %s', ', '.join(found_items))
+            logger.info("Found Info: %s", ", ".join(found_items))
 
             insert_ticket_info(
                 timestamp,
-                info.get('line'),
-                info.get('station'),
-                info.get('direction'),
+                info.get("line"),
+                info.get("station"),
+                info.get("direction"),
             )
     else:
-        logger.info('No line, station or direction found in the message')
+        logger.info("No line, station or direction found in the message")
 
 
 def handle_exception(exc_type, exc_value, exc_traceback):
     """Handle uncaught exceptions by sending a POST request with exception info."""
-    error_message = "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+    error_message = "".join(
+        traceback.format_exception(exc_type, exc_value, exc_traceback)
+    )
     requests.post(WATCHER_URL, json={"error_message": error_message})
-    logger.error('Unhandled exception', exc_info=(exc_type, exc_value, exc_traceback))
+    logger.error("Unhandled exception", exc_info=(exc_type, exc_value, exc_traceback))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logger = setup_logger()
+
+    NLP_BOT_TOKEN = "6515475322:AAGj9UvD4MoiuWzPBySF4RRWntSvWagTgVs"
     sys.excepthook = handle_exception
     utc = pytz.UTC
 
-    logger.info('Bot is running...')
+    logger.info("Bot is running...")
 
     @nlp_bot.message_handler(func=lambda message: message)
     def get_info(message):
-        logger.info('------------------------')
-        logger.info('MESSAGE RECEIVED')
-        
+        logger.info("------------------------")
+        logger.info("MESSAGE RECEIVED")
+
         timestamp = datetime.fromtimestamp(message.date, utc)
         # Round the timestamp to the last minute
         timestamp = timestamp.replace(second=0, microsecond=0)
-            
+
         process_new_message(timestamp, message)
 
     bot_thread = threading.Thread(target=start_bot)
 
     bot_thread.start()
 
-    logger.info('Starting the nlp bot...')
-    
+    logger.info("Starting the nlp bot...")
+
     app.run(port=5001)
