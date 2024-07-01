27cc46c8d26a56291bc3b58489e476f199e80009
David Brandes <brandes.1@campus.tu-berlin.de>
2024-07-01 21:55:00 +0200

    port change

--- packages/telegram_bots/FreiFahren_BE_NLP/main.py
+++ packages/telegram_bots/FreiFahren_BE_NLP/main.py
@@ -120,6 +120,6 @@ if __name__ == "__main__":
     logger.info("Waitress serve NLP_BOT")
 
     from waitress import serve
-    serve(nlp_app, host='0.0.0.0', port=5001)
+    serve(nlp_app, host='127.0.0.1', port=6001)
     
 
