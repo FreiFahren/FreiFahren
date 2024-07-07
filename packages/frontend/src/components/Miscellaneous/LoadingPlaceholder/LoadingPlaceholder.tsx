4c5cca89d5951132a22ece87543ec39a123d8f7d
Johan Trieloff <johan@trieloff.net>
2024-07-07 19:52:58 +0200

    docs: document the usage of the component

--- packages/frontend/src/components/Miscellaneous/LoadingPlaceholder/LoadingPlaceholder.tsx
+++ packages/frontend/src/components/Miscellaneous/LoadingPlaceholder/LoadingPlaceholder.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 
 import './LoadingPlaceholder.css';
 
+/**
+ * LoadingPlaceholder Component
+ *
+ * This component renders a placeholder with a loading animation.
+ * It's designed to be used in place of content that is still loading.
+ *
+ * The component creates a gray rectangular area with a subtle shimmering
+ * animation to indicate a loading state.
+ *
+ * In order to use this anywhere please make sure it has a parent element with a sensible width and height.
+*/
 const LoadingPlaceholder: React.FC = () => {
     return (
         <div className='loading-placeholder'>
