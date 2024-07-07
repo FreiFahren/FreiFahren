a4d49696c9d734c125a4e15fa4e0710d935dedd7
Johan Trieloff <johan@trieloff.net>
2024-07-07 12:06:50 +0200

    feat: include basic profiles

--- packages/frontend/src/components/Modals/FeedbackModal/FeedbackModal.tsx
+++ packages/frontend/src/components/Modals/FeedbackModal/FeedbackModal.tsx
@@ -6,15 +6,65 @@ interface FeedbackModalProps {
     openAnimationClass?: string;
 }
 
+const github_icon = `${process.env.PUBLIC_URL}/icons/github.svg`;
+
 const FeedbackModal: React.FC<FeedbackModalProps> = ({ openAnimationClass }) => {
     return (
         <div className={`feedback-modal modal container ${openAnimationClass}`}>
-            <h1>Sende uns dein Feedback</h1>
+            <h1>Sende dein Feedback an:</h1>
             <ul>
-                <li><p>David</p></li>
-                <li><p>Joff</p></li>
-                <li><p>Moritz</p></li>
-                <li><p>Johan</p></li>
+                <li>
+                    <img
+                    className='profile-picture'
+                    src={process.env.PUBLIC_URL + '/icons/profiles/moritz.jpeg'}
+                    alt='Moritz Github Profile Picture'
+                    />
+                    <p>Moritz</p>
+                    <div>
+                        <a href='https://github.com/mclrc' target='_blank' rel='noopener noreferrer'>
+                            <img src={github_icon} alt='github icon'/>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <img
+                    className='profile-picture'
+                    src={process.env.PUBLIC_URL + '/icons/profiles/joff.jpeg'}
+                    alt='Joff Github Profile Picture'
+                    />
+                    <p>Joff</p>
+                    <div>
+                        <a href='hhttps://github.com/jfsalzmann' target='_blank' rel='noopener noreferrer'>
+                            <img src={github_icon} alt='github icon'/>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <img
+                    className='profile-picture'
+                    src={process.env.PUBLIC_URL + '/icons/profiles/johan.jpeg'}
+                    alt='Johan Github Profile Picture'
+                    />
+                    <p>Johan</p>
+                    <div>
+                        <a href='https://github.com/johan-t' target='_blank' rel='noopener noreferrer'>
+                            <img src={github_icon} alt='github icon'/>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <img
+                    className='profile-picture'
+                    src={process.env.PUBLIC_URL + '/icons/profiles/david.jpeg'}
+                    alt='David Github Profile Picture'
+                    />
+                    <p>David</p>
+                    <div>
+                        <a href='https://github.com/brandesdavid' target='_blank' rel='noopener noreferrer'>
+                            <img src={github_icon} alt='github icon'/>
+                        </a>
+                    </div>
+                </li>
             </ul>
             <h2>Über uns</h2>
             <p>
