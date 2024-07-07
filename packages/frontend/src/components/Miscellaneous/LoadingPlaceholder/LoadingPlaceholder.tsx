b10b6f022312eaeda2c9b01468fac927b58df435
Johan Trieloff <johan@trieloff.net>
2024-07-07 19:11:58 +0200

    feat: introduce loading placeholder component

---
+++ packages/frontend/src/components/Miscellaneous/LoadingPlaceholder/LoadingPlaceholder.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './LoadingPlaceholder.css';
+
+const LoadingPlaceholder: React.FC = () => {
+    return (
+        <div className='loading-placeholder'/>
+    );
+};
+
+export default LoadingPlaceholder;
