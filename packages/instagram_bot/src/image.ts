6ab81a305ef16252451e6292bfaf81ae4a47d564
Johan Trieloff <johan@trieloff.net>
2024-08-19 18:03:09 +0200

    refactor: improved styling of the image

--- packages/instagram_bot/src/image.ts
+++ packages/instagram_bot/src/image.ts
@@ -1,40 +1,68 @@
 import sharp from 'sharp'
 import { type Inspector } from './models'
+import fs from 'fs/promises'
+import path from 'path'
+
+function estimateTextWidth(text: string, fontSize: number): number {
+    // This is a rough estimate. Adjust the multiplier as needed for your font.
+    return text.length * fontSize * 0.6
+}
 
 export async function createImage(inspectors: Inspector[]): Promise<Buffer> {
     console.log('Creating image with inspectors:', inspectors)
     const width = 1080
     const height = 1920
 
+    // Read the Raleway font file
+    const fontPath = path.join(__dirname, '../fonts/Raleway/static/Raleway-Regular.ttf')
+    const fontData = await fs.readFile(fontPath)
+    const fontBase64 = fontData.toString('base64')
+
     const inspectorSvgs = inspectors
         .map((inspector, index) => {
-            let yOffset = index * 150
+            let yOffset = 250 + index * 100
+            const stationWidth = estimateTextWidth(inspector.station.name, 40)
+            let lineX = stationWidth + 120 // 20px gap after station name
+            let directionX = lineX
+
             let svg = `
                 <g transform="translate(0, ${yOffset})">
-                    <text x="50" y="100" font-family="Arial" font-size="30" fill="white">Station: ${inspector.station.name}</text>
+                    <circle cx="70" cy="0" r="8" fill="white" />
+                    <text x="100" y="10" font-family="Raleway" font-size="40" font-weight="bold" fill="white">${inspector.station.name}</text>
             `
+
             if (inspector.line) {
-                svg += `<text x="50" y="140" font-family="Arial" font-size="30" fill="white">Linie: ${inspector.line}</text>`
-                yOffset += 40
+                svg += `<text x="${lineX}" y="10" font-family="Raleway" font-size="40" fill="white">${inspector.line}</text>`
+                directionX = lineX + estimateTextWidth(inspector.line, 40) + 20 // 20px gap after line
             }
+
             if (inspector.direction && inspector.direction.name) {
-                svg += `<text x="50" y="${yOffset + 140}" font-family="Arial" font-size="30" fill="white">Richtung: ${
-                    inspector.direction.name
-                }</text>`
+                svg += `<text x="${directionX}" y="10" font-family="Raleway" font-size="40" fill="white">${inspector.direction.name}</text>`
             }
+
             svg += `</g>`
             return svg
         })
         .join('')
 
     const svg = `
-        <svg width="${width}" height="${height}">
-            <rect width="100%" height="100%" fill="black"/>
-            <text x="50" y="50" font-family="Arial" font-size="40" fill="white">Ticket Inspectors Report</text>
+        <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+                <style>
+                    @font-face {
+                        font-family: 'Raleway';
+                        src: url(data:application/font-ttf;charset=utf-8;base64,${fontBase64}) format('truetype');
+                        font-weight: normal;
+                        font-style: normal;
+                    }
+                </style>
+            </defs>
+            <rect width="100%" height="100%" fill="#232323"/>
+            <text x="70" y="120" font-family="Raleway" font-size="80" font-weight="bold" fill="white">Aktuelle Meldungen</text>
             ${inspectorSvgs}
-            <text x="50" y="${
-                height - 20
-            }" font-family="Arial" font-size="20" fill="white">Mehr Info auf app.freigfahren.org</text>
+            <text x="70" y="${
+                height - 60
+            }" font-family="Raleway" font-size="40" fill="white">Mehr Infos auf <tspan text-decoration="underline">app.freifahren.org</tspan></text>
         </svg>
     `
 
