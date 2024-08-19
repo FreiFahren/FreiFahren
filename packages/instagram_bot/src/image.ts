256495e0ab845cd209c70a5d99e0a34caf2ae210
Johan Trieloff <johan@trieloff.net>
2024-08-19 18:20:24 +0200

    refactor: use lineColors to color the lines
    I created a slight Background of the line, similar to how it looks in the web app.

--- packages/instagram_bot/src/image.ts
+++ packages/instagram_bot/src/image.ts
@@ -4,7 +4,6 @@ import fs from 'fs/promises'
 import path from 'path'
 
 function estimateTextWidth(text: string, fontSize: number): number {
-    // This is a rough estimate. Adjust the multiplier as needed for your font.
     return text.length * fontSize * 0.6
 }
 
@@ -13,16 +12,43 @@ export async function createImage(inspectors: Inspector[]): Promise<Buffer> {
     const width = 1080
     const height = 1920
 
-    // Read the Raleway font file
     const fontPath = path.join(__dirname, '../fonts/Raleway/static/Raleway-Regular.ttf')
     const fontData = await fs.readFile(fontPath)
     const fontBase64 = fontData.toString('base64')
+
+    const lineColors = {
+        S1: '#da6ba2',
+        S2: '#007734',
+        S3: '#0066ad',
+        S5: '#eb7405',
+        S7: '#816da6',
+        S8: '#66aa22',
+        S9: '#992746',
+        S41: '#ad5937',
+        S42: '#cb6418',
+        S45: '#cd9c53',
+        S46: '#cd9c53',
+        S47: '#cd9c53',
+        S25: '#007734',
+        S26: '#007734',
+        S75: '#816da6',
+        S85: '#66aa22',
+        U1: '#7DAD4C',
+        U2: '#DA421E',
+        U3: '#16683D',
+        U4: '#F0D722',
+        U5: '#7E5330',
+        U6: '#8C6DAB',
+        U7: '#528DBA',
+        U8: '#224F86',
+        U9: '#F3791D',
+    }
 
     const inspectorSvgs = inspectors
         .map((inspector, index) => {
             let yOffset = 250 + index * 100
             const stationWidth = estimateTextWidth(inspector.station.name, 40)
-            let lineX = stationWidth + 120 // 20px gap after station name
+            let lineX = stationWidth + 120
             let directionX = lineX
 
             let svg = `
@@ -32,8 +58,22 @@ export async function createImage(inspectors: Inspector[]): Promise<Buffer> {
             `
 
             if (inspector.line) {
-                svg += `<text x="${lineX}" y="10" font-family="Raleway" font-size="40" fill="white">${inspector.line}</text>`
-                directionX = lineX + estimateTextWidth(inspector.line, 40) + 20 // 20px gap after line
+                const lineColor = lineColors[inspector.line as keyof typeof lineColors] || '#ffffff'
+                const lineWidth = estimateTextWidth(inspector.line, 40)
+                const lineBgWidth = lineWidth + 20 // Add some padding
+                const lineBgHeight = 50 // Adjust as needed
+
+                svg += `
+                    <rect x="${
+                        lineX - 2.5
+                    }" y="-30" width="${lineBgWidth}" height="${lineBgHeight}" rx="8" ry="8" fill="${lineColor}" />
+                    <text x="${
+                        lineX + lineBgWidth / 2
+                    }" y="10" font-family="Raleway" font-size="40" font-weight="bold" fill="white" text-anchor="middle">${
+                    inspector.line
+                }</text>
+                `
+                directionX = lineX + lineBgWidth + 40 // 20px gap after line
             }
 
             if (inspector.direction && inspector.direction.name) {
