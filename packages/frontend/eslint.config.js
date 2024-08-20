8f6b48b17b580f6cda59a6aeb902e68749e4e257
Johan Trieloff <johan@trieloff.net>
2024-08-20 13:52:04 +0200

    chore: remove eslint styling rules
    as we are now using prettier as a formatter we dont need to use the linter as a formater.

--- packages/frontend/eslint.config.js
+++ packages/frontend/eslint.config.js
@@ -10,10 +10,6 @@ export default tseslint.config(
       'react-hooks': reactHooksPlugin,
     },
     rules: {
-      'quotes': ['error', 'single'],
-      'jsx-quotes': ['error', 'prefer-single'],
-      'no-trailing-spaces': ['error'],
-      'no-multiple-empty-lines': ['error', { 'max': 1 }],
       'no-console': ['error', { allow: ['warn', 'error'] }],
       'react-hooks/exhaustive-deps': 'warn',
     }
--- packages/frontend/src/components/Modals/MarkerModal/MarkerModal.tsx
+++ packages/frontend/src/components/Modals/MarkerModal/MarkerModal.tsx
@@ -1,59 +1,61 @@
 import React, { useEffect, useState, useMemo } from 'react'
 
 import { MarkerData } from '../../Map/Markers/MarkerContainer'
-import { elapsedTimeMessage, stationDistanceMessage } from '../../../utils/mapUtils';
-import { getStationDistance } from '../../../utils/dbUtils';
-import Skeleton, {useSkeleton} from '../../Miscellaneous/LoadingPlaceholder/Skeleton';
+import { elapsedTimeMessage, stationDistanceMessage } from '../../../utils/mapUtils'
+import { getStationDistance } from '../../../utils/dbUtils'
+import Skeleton, { useSkeleton } from '../../Miscellaneous/LoadingPlaceholder/Skeleton'
 import './MarkerModal.css'
 
 interface MarkerModalProps {
-  selectedMarker: MarkerData,
-  className: string,
-  userLat?: number,
-  userLng?: number,
-  children?: React.ReactNode
+    selectedMarker: MarkerData
+    className: string
+    userLat?: number
+    userLng?: number
+    children?: React.ReactNode
 }
 
 const MarkerModal: React.FC<MarkerModalProps> = ({ className, children, selectedMarker, userLat, userLng }) => {
-  const { timestamp, station, line, direction, isHistoric } = selectedMarker;
+    const { timestamp, station, line, direction, isHistoric } = selectedMarker
 
-  const adjustedTimestamp = useMemo(() => {
-    const tempTimestamp = new Date(timestamp);
-    return tempTimestamp;
-  }, [timestamp]);
-  const currentTime = new Date().getTime();
-  const elapsedTime = currentTime - adjustedTimestamp.getTime();
+    const adjustedTimestamp = useMemo(() => {
+        const tempTimestamp = new Date(timestamp)
+        return tempTimestamp
+    }, [timestamp])
+    const currentTime = new Date().getTime()
+    const elapsedTime = currentTime - adjustedTimestamp.getTime()
 
-  // isLoading is used to avoid showing the placeholder when the location is not available
-  const [isLoading, setIsLoading] = useState(false);
-  const showSkeleton = useSkeleton({ isLoading });
+    // isLoading is used to avoid showing the placeholder when the location is not available
+    const [isLoading, setIsLoading] = useState(false)
+    const showSkeleton = useSkeleton({ isLoading })
 
-  const [stationDistance, setStationDistance] = useState<number | null>(null);
-  useEffect(() => {
-    const fetchDistance = async () => {
-        setIsLoading(true);
-        const distance = await getStationDistance(userLat, userLng, station.id);
-        setStationDistance(distance);
-        setIsLoading(false);
-    };
+    const [stationDistance, setStationDistance] = useState<number | null>(null)
+    useEffect(() => {
+        const fetchDistance = async () => {
+            setIsLoading(true)
+            const distance = await getStationDistance(userLat, userLng, station.id)
+            setStationDistance(distance)
+            setIsLoading(false)
+        }
 
-    fetchDistance();
-}, [userLat, userLng, station.id]);
+        fetchDistance()
+    }, [userLat, userLng, station.id])
 
-  return (
-    <div className={`marker-modal info-popup modal ${className}`}>
-      {children}
-      <h1>{station.name}</h1>
-      {(direction.name !== '' || line !== '' ) && <h2><span className={line}>{line}</span> {direction.name}</h2> }
-      <div>
-        <p>{elapsedTimeMessage(elapsedTime, isHistoric)}</p>
-        <p className='distance'>
-          {showSkeleton ? <Skeleton /> : stationDistanceMessage(stationDistance)}
-        </p>
-        {selectedMarker.message && <p className='description'>{selectedMarker.message}</p>}
-      </div>
-    </div>
-  )
+    return (
+        <div className={`marker-modal info-popup modal ${className}`}>
+            {children}
+            <h1>{station.name}</h1>
+            {(direction.name !== '' || line !== '') && (
+                <h2>
+                    <span className={line}>{line}</span> {direction.name}
+                </h2>
+            )}
+            <div>
+                <p>{elapsedTimeMessage(elapsedTime, isHistoric)}</p>
+                <p className="distance">{showSkeleton ? <Skeleton /> : stationDistanceMessage(stationDistance)}</p>
+                {selectedMarker.message && <p className="description">{selectedMarker.message}</p>}
+            </div>
+        </div>
+    )
 }
 
 export default MarkerModal
