8786ed3726e17acf825f0e58f99eb59cdf0b1737
Johan Trieloff <johan@trieloff.net>
2024-08-29 21:49:30 +0200

    refactor: remove all reportform code
    the component will be rewritten from the ground up so I have removed all of the old code to make it easier to write a new component

--- packages/frontend/src/utils/uiUtils.tsx
+++ packages/frontend/src/utils/uiUtils.tsx
@@ -1,16 +1,4 @@
-import { selectOption } from '../components/Form/AutocompleteInputForm/AutocompleteInputForm'
 import { useState } from 'react'
-
-type StationsList = {
-    [key: string]: {
-        name: string
-        lines: string[]
-    }
-}
-
-type LinesList = {
-    [key: string]: string[]
-}
 
 export function highlightElement(id: string) {
     const element = document.getElementById(id)
@@ -31,61 +19,6 @@ export function highlightElement(id: string) {
                 elementClass[0].classList.remove('highlight')
             }, 3000)
         }
-    }
-}
-
-// when a station is selected, the line options are redefined, giving only the lines that the station is connected to
-export const redefineLineOptions = (option: selectOption, stationsList: StationsList): selectOption[] => {
-    const newLineOptions = Object.entries(stationsList)
-        .filter(([station_id]) => station_id === option.value)
-        .flatMap(([, station]) => station.lines.map((line) => ({ value: line, label: line })))
-
-    return newLineOptions
-}
-
-// when a line is selected, the station options are redefined, giving only the stations that the line is connected to
-export const redefineStationOptions = (
-    option: selectOption,
-    linesList: LinesList,
-    stationsList: StationsList
-): selectOption[] => {
-    const newStationOptions: selectOption[] = []
-
-    for (const station_id of linesList[option.value]) {
-        newStationOptions.push({ value: station_id, label: stationsList[station_id].name })
-    }
-
-    return newStationOptions
-}
-
-// when a line is selected, the direction options are redefined, giving only the first and last station of the line
-export const redefineDirectionOptions = (
-    option: selectOption,
-    linesList: LinesList,
-    stationsList: StationsList
-): selectOption[] => {
-    if (!option || !option.value || !linesList || !stationsList) {
-        return []
-    }
-
-    const length = linesList[option.value].length
-    const { 0: firstStationId, [length - 1]: lastStationId } = linesList[option.value]
-    const newDirectionOptions = [
-        { value: firstStationId, label: stationsList[firstStationId].name },
-        { value: lastStationId, label: stationsList[lastStationId].name },
-    ]
-
-    return newDirectionOptions
-}
-
-export function createWarningSpan(elementId: string, message: string) {
-    let warningSpan = document.getElementById('warning-span')
-    if (!warningSpan) {
-        warningSpan = document.createElement('span')
-        warningSpan.id = 'warning-span'
-        warningSpan.className = 'red-highlight'
-        warningSpan.textContent = message
-        document.getElementById(elementId)?.appendChild(warningSpan)
     }
 }
 
@@ -118,19 +51,6 @@ export function setColorThemeInLocalStorage() {
         localStorage.setItem('colorTheme', 'light')
     } else {
         localStorage.setItem('colorTheme', 'dark')
-    }
-}
-
-export const handleTextareaInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const target = event.target as HTMLTextAreaElement
-    target.style.height = 'auto'
-    const maxHeight = window.innerHeight * 0.35 // Set it based on the screen height
-
-    if (target.scrollHeight > maxHeight) {
-        target.style.height = `${maxHeight}px`
-        target.style.overflowY = 'auto'
-    } else {
-        target.style.height = `${target.scrollHeight}px`
     }
 }
 
