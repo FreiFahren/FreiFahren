8c5756621bcf28a5b6660a6ecdae54089dca2b4f
Johan Trieloff <johan@trieloff.net>
2024-09-17 17:06:54 +0200

    feat: e2e test /data/id

---
+++ packages/backend/end_to_end_tests/id_test.go
@@ -0,0 +1,72 @@
+package end_to_end_tests
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/FreiFahren/backend/server"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetStationIdEndpoint(t *testing.T) {
+	e := server.SetupServer()
+
+	tests := []struct {
+		name           string
+		stationName    string
+		expectedStatus int
+		expectedID     string
+	}{
+		{
+			name:           "Valid Station",
+			stationName:    "Alexanderplatz",
+			expectedStatus: http.StatusOK,
+			expectedID:     "SU-A",
+		},
+		{
+			name:           "Station Not Found",
+			stationName:    "UnknownStation",
+			expectedStatus: http.StatusNotFound,
+			expectedID:     "",
+		},
+		{
+			name:           "Case Insensitive",
+			stationName:    "alexanderplatz",
+			expectedStatus: http.StatusOK,
+			expectedID:     "SU-A",
+		},
+		{
+			name:           "Whitespace Insensitive",
+			stationName:    "Alexander platz",
+			expectedStatus: http.StatusOK,
+			expectedID:     "SU-A",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/data/id", nil)
+
+			q := req.URL.Query()
+			q.Add("name", tc.stationName)
+			req.URL.RawQuery = q.Encode()
+
+			// Create a ResponseRecorder to capture the response
+			rec := httptest.NewRecorder()
+
+			e.ServeHTTP(rec, req)
+
+			assert.Equal(t, tc.expectedStatus, rec.Code)
+
+			if tc.expectedStatus == http.StatusOK {
+				expectedBody := `{"id":"` + tc.expectedID + `"}`
+				assert.JSONEq(t, expectedBody, rec.Body.String())
+			} else {
+				// Handle error response
+				expectedBody := `{"error":"Station not found"}`
+				assert.JSONEq(t, expectedBody, rec.Body.String())
+			}
+		})
+	}
+}
