ec926f9ea61364e50fe57f308681a177ff006d4c
Johan Trieloff <johan@trieloff.net>
2024-09-27 18:01:42 +0200

    feat: sort lines
    the lines are now being sorted in a sensible way. The Ubahns are before the Sbahns and the S41, S42 are the first Sbahns. This will still make it easy to use the list as they are still sorted by their number but also some exceptions based on how often they are being reported. This should make it easier to report an inspector.

--- packages/frontend/src/contexts/StationsAndLinesContext.tsx
+++ packages/frontend/src/contexts/StationsAndLinesContext.tsx
@@ -18,7 +18,33 @@ export const StationsAndLinesProvider: React.FC<React.PropsWithChildren> = ({ ch
         const fetchLinesAndStations = async () => {
             try {
                 const [lines, stations] = await Promise.all([getAllLinesList(), getAllStationsList()])
-                setAllLines(lines)
+
+                // sort them because Ubahns are more common than Sbahns, thus they have to first
+                const sortedAllLines = () => {
+                    return Object.fromEntries(
+                        Object.entries(lines).sort(([lineA], [lineB]) => {
+                            const typeA = lineA[0]
+                            const typeB = lineB[0]
+                            const numberA = parseInt(lineA.slice(1))
+                            const numberB = parseInt(lineB.slice(1))
+
+                            if (typeA !== typeB) {
+                                return typeB.localeCompare(typeA) // U comes before S
+                            }
+
+                            // Ringbahn should be first Sbahn because they are the most common Sbahns
+                            if (typeA === 'S' && typeB === 'S') {
+                                if (lineA === 'S41') return -1
+                                if (lineB === 'S41') return 1
+                                if (lineA === 'S42') return -1
+                                if (lineB === 'S42') return 1
+                            }
+
+                            return numberA - numberB // Sort by number
+                        })
+                    )
+                }
+                setAllLines(sortedAllLines)
                 setAllStations(stations)
             } catch (error) {
                 console.error('Failed to fetch lines and stations:', error)
