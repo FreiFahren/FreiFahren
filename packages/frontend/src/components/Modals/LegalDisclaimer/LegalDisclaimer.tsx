397f20b6846e12bd30b7abce2e56c7d75cc2c59d
Johan Trieloff <johan@trieloff.net>
2024-09-28 16:56:34 +0200

    feat: always show submit button on bottom of modal

--- packages/frontend/src/components/Modals/LegalDisclaimer/LegalDisclaimer.tsx
+++ packages/frontend/src/components/Modals/LegalDisclaimer/LegalDisclaimer.tsx
@@ -21,38 +21,42 @@ const LegalDisclaimer: React.FC<LegalDisclaimerProps> = ({ closeModal, openAnima
     }
     return (
         <div className={`legal-disclaimer container modal ${openAnimationClass}`} id="legal-disclaimer">
-            <h1>Bitte bestätigen Sie vor dem Fortfahren</h1>
-            <p>
-                Um die Integrität unserer Community und den Geist fairer Nutzung zu wahren, bitten wir Sie, zwei
-                wichtige Punkte zu bestätigen:
-            </p>
-            <ol>
-                <li>
-                    <strong>Ich besitze ein gültiges Ticket für meine Reise.</strong>
+            <div className="content">
+                <h1>Bitte bestätigen Sie vor dem Fortfahren</h1>
+                <section>
                     <p>
-                        Ich verstehe, dass diese App dazu dient, das Bewusstsein und die Planung im öffentlichen
-                        Nahverkehr zu verbessern, nicht aber um Regeln oder Vorschriften zu umgehen.
+                        Um die Integrität unserer Community und den Geist fairer Nutzung zu wahren, bitten wir Sie, zwei
+                        wichtige Punkte zu bestätigen:
                     </p>
-                </li>
-                <li>
-                    <strong>Ich nutze die App nicht aktiv während der Fahrt.</strong>
-                    <p>
-                        Mir ist bewusst, dass die aktive Nutzung der App während der Fahrt andere Fahrgäste stören und
-                        gegen die Nutzungsbedingungen des Verkehrsbetriebs verstoßen kann.
-                    </p>
-                </li>
-            </ol>
-            <div>
-                <button onClick={handleClose}>Ich bestätige</button>
+                    <ol>
+                        <li>
+                            <strong>Ich besitze ein gültiges Ticket für meine Reise.</strong>
+                            <p>
+                                Ich verstehe, dass diese App dazu dient, das Bewusstsein und die Planung im öffentlichen
+                                Nahverkehr zu verbessern, nicht aber um Regeln oder Vorschriften zu umgehen.
+                            </p>
+                        </li>
+                        <li>
+                            <strong>Ich nutze die App nicht aktiv während der Fahrt.</strong>
+                            <p>
+                                Mir ist bewusst, dass die aktive Nutzung der App während der Fahrt andere Fahrgäste
+                                stören und gegen die Nutzungsbedingungen des Verkehrsbetriebs verstoßen kann.
+                            </p>
+                        </li>
+                    </ol>
+                </section>
             </div>
-            <ul className="align-child-on-line">
-                <li>
-                    <Link to="/impressum">Impressum</Link>
-                </li>
-                <li>
-                    <Link to="/Datenschutz">Datenschutz</Link>
-                </li>
-            </ul>
+            <div className="footer">
+                <button onClick={handleClose}>Ich bestätige</button>
+                <ul className="align-child-on-line">
+                    <li>
+                        <Link to="/impressum">Impressum</Link>
+                    </li>
+                    <li>
+                        <Link to="/Datenschutz">Datenschutz</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
