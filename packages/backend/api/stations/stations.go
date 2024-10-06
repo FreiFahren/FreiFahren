3b181f745bf5fe10683e7f98f915002365940631
Johan Trieloff <johan@trieloff.net>
2024-10-06 15:09:27 +0200

    refactor: query by name from url

--- packages/backend/api/stations/stations.go
+++ packages/backend/api/stations/stations.go
@@ -2,6 +2,7 @@ package stations
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/FreiFahren/backend/data"
 	"github.com/labstack/echo/v4"
@@ -17,6 +18,24 @@ func GetSingleStation(c echo.Context) error {
 
 	if station, ok := stations[stationId]; ok {
 		return c.JSON(http.StatusOK, station)
+	}
+
+	return c.JSON(http.StatusNotFound, "Station not found: The specified station does not exist.")
+}
+
+func SearchStation(c echo.Context) error {
+	name := c.QueryParam("name")
+	if name == "" {
+		return c.JSON(http.StatusBadRequest, "Name is required")
+	}
+
+	stations := data.GetStationsList()
+	normalizedName := strings.ToLower(strings.TrimSpace(name))
+
+	for _, station := range stations {
+		if strings.ToLower(strings.TrimSpace(station.Name)) == normalizedName {
+			return c.JSON(http.StatusOK, station)
+		}
 	}
 
 	return c.JSON(http.StatusNotFound, "Station not found: The specified station does not exist.")
