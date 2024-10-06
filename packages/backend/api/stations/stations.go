ce9159f890afdc4aef355e8c4106fa6318f763a0
Johan Trieloff <johan@trieloff.net>
2024-10-06 15:42:46 +0200

    docs: document /stations

--- packages/backend/api/stations/stations.go
+++ packages/backend/api/stations/stations.go
@@ -5,13 +5,42 @@ import (
 	"strings"
 
 	"github.com/FreiFahren/backend/data"
+	_ "github.com/FreiFahren/backend/docs"
 	"github.com/labstack/echo/v4"
 )
 
+// @Summary Get all stations
+//
+// @Description Retrieves information about all available stations.
+// @Description This endpoint returns a list of all stations and their details.
+//
+// @Tags stations
+//
+// @Produce json
+//
+// @Success 200 {object} map[string]utils.StationListEntry
+// @Failure 500 "Internal Server Error: Error retrieving stations data."
+//
+// @Router /stations [get]
 func GetAllStations(c echo.Context) error {
 	return c.JSON(http.StatusOK, data.GetStationsList())
 }
 
+// @Summary Get a single station by ID
+//
+// @Description Retrieves information about a specific station based on its ID.
+// @Description This endpoint returns the details of a single station.
+//
+// @Tags stations
+//
+// @Produce json
+//
+// @Param stationId path string true "ID of the station"
+//
+// @Success 200 {object} utils.StationListEntry "Successfully retrieved the specified station data."
+// @Failure 404 {object} map[string]string "Station not found: The specified station does not exist."
+//
+// @Router /stations/{stationId} [get]
 func GetSingleStation(c echo.Context) error {
 	stationId := c.Param("stationId")
 	stations := data.GetStationsList()
@@ -23,10 +52,26 @@ func GetSingleStation(c echo.Context) error {
 	return c.JSON(http.StatusNotFound, "Station not found: The specified station does not exist.")
 }
 
+// @Summary Search for a station by name
+//
+// @Description Searches for a station using the provided name and returns the matching station information.
+// @Description This endpoint is case and whitespace insensitive and returns the first exact match found.
+//
+// @Tags stations
+//
+// @Produce json
+//
+// @Param name query string true "Name of the station to search for"
+//
+// @Success 200 {object} utils.StationListEntry "Successfully found and retrieved the station data."
+// @Failure 400 {object} map[string]string "Bad Request: Missing station name parameter."
+// @Failure 404 {object} map[string]string "Station not found: No station matches the provided name."
+//
+// @Router /stations/search [get]
 func SearchStation(c echo.Context) error {
 	name := c.QueryParam("name")
 	if name == "" {
-		return c.JSON(http.StatusBadRequest, "Name is required")
+		return c.JSON(http.StatusBadRequest, "Bad Request: Missing station name parameter.")
 	}
 
 	stations := data.GetStationsList()
@@ -38,5 +83,5 @@ func SearchStation(c echo.Context) error {
 		}
 	}
 
-	return c.JSON(http.StatusNotFound, "Station not found: The specified station does not exist.")
+	return c.JSON(http.StatusNotFound, "Station not found: No station matches the provided name.")
 }
