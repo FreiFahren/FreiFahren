b1bca99c4003b24482d4475283f914239541d031
Johan Trieloff <johan@trieloff.net>
2024-10-16 20:07:15 +0200

    feat: Avoid returning `True` for "Sexist Hatespeech"
    The model is not good at detecting sexist hatespeech and the metrics are only acceptable if we exclude it.

--- packages/hate_speech_filter/hatespeechfilter.py
+++ packages/hate_speech_filter/hatespeechfilter.py
@@ -4,22 +4,26 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipe
 app = Flask(__name__)
 
 tokenizer = AutoTokenizer.from_pretrained("chrisrtt/gbert-multi-class-german-hate")
-model = AutoModelForSequenceClassification.from_pretrained("chrisrtt/gbert-multi-class-german-hate")
-classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+model = AutoModelForSequenceClassification.from_pretrained(
+    "chrisrtt/gbert-multi-class-german-hate"
+)
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
 
-@app.route('/classification', methods=['POST'])
+
+@app.route("/classification", methods=["POST"])
 def classify_text():
     data = request.get_json()
-    
-    if 'text' not in data:
-        return jsonify({"error": "No text provided"}), 400
-    
-    text = data['text']
-    
-    result = classifier(text)
-    
-    return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9090)
-    
+    if "text" not in data:
+        return jsonify({"error": "No text provided"}), 400
+
+    text = data["text"]
+
+    result = classifier(text)
+
+    # Return True if it is hatespeech, False otherwise
+    return jsonify(result[0]["label"] not in ["No Hate Speech", "Sexist Hate Speech"])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9090)
