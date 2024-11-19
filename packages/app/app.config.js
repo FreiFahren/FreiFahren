e49571a4492b236b5fb771c41aa9d3308526997f
Moritz <moritzamando@proton.me>
2024-11-19 17:25:04 +0100

    Don't try to upload apks

--- packages/app/app.config.js
+++ packages/app/app.config.js
@@ -3,7 +3,7 @@ export default {
     expo: {
         name: 'freifahren',
         slug: 'Freifahren',
-        version: '0.0.1',
+        version: '0.0.2',
         orientation: 'portrait',
         icon: './assets/app-icon.png',
         userInterfaceStyle: 'dark',
