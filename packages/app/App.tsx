616f1eb4ee29eb1897cfdd2aa653ab97d2313da0
Moritz <moritzamando@proton.me>
2024-11-20 10:23:06 +0100

    Add sentry to app

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,6 +1,7 @@
 import 'intl-pluralrules'
 
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+import { init, wrap as sentryWrap } from '@sentry/react-native'
 import { QueryClientProvider } from '@tanstack/react-query'
 // eslint-disable-next-line import/no-namespace
 import * as NavigationBar from 'expo-navigation-bar'
@@ -14,9 +15,15 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { queryClient } from './src/api'
+import { config } from './src/config'
 import i18n from './src/i18n'
 import { Main } from './src/Main'
 import { theme } from './src/theme'
+
+init({
+    dsn: config.SENTRY_DSN,
+    debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+})
 
 const App = () => {
     useEffect(() => {
@@ -49,4 +56,4 @@ const App = () => {
 }
 
 // eslint-disable-next-line import/no-default-export
-export default App
+export default sentryWrap(App)
--- packages/app/src/config.ts
+++ packages/app/src/config.ts
@@ -7,11 +7,13 @@ export const config = (() => {
                 FF_API_BASE_URL: z.string(),
                 MAP_STYLE_URL: z.string(),
                 PRIVACY_POLICY_URL: z.string(),
+                SENTRY_DSN: z.string(),
             })
             .parse({
                 FF_API_BASE_URL: process.env.EXPO_PUBLIC_FF_API_BASE_URL,
                 MAP_STYLE_URL: process.env.EXPO_PUBLIC_MAP_STYLE_URL,
                 PRIVACY_POLICY_URL: process.env.EXPO_PUBLIC_PRIVACY_POLICY_URL,
+                SENTRY_DSN: process.env.EXPO_PUBLIC_SENTRY_DSN,
             })
     } catch (error) {
         // eslint-disable-next-line no-console
