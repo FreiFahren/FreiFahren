615088297ab81c66caa902b7610c16628a997827
Moritz <moritzamando@proton.me>
2024-11-21 23:46:16 +0100

    Commit missing folder

---
+++ packages/app/src/components/Blocker/Disclaimer.tsx
@@ -0,0 +1,75 @@
+import { BottomSheetModal } from '@gorhom/bottom-sheet'
+import { noop } from 'lodash'
+import { DateTime } from 'luxon'
+import { Box, Text, View } from 'native-base'
+import { forwardRef, Ref } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Linking } from 'react-native'
+
+import { useAppStore } from '../../app.store'
+import { config } from '../../config'
+import { track } from '../../tracking'
+import { FFButton } from '../common/FFButton'
+import { FFScrollSheet } from '../common/FFSheet'
+
+type DisclaimerProps = {
+    onDismiss: () => void
+}
+
+export const Disclaimer = forwardRef(({ onDismiss }: DisclaimerProps, ref: Ref<BottomSheetModal>) => {
+    const { t } = useTranslation('disclaimer')
+
+    const updateStore = useAppStore(({ update }) => update)
+
+    const handleDismiss = () => {
+        track({ name: 'Disclaimer Dismissed' })
+        updateStore({ dismissedDisclaimerAt: DateTime.now().toISO() })
+        onDismiss()
+    }
+
+    const openPrivacyPolicy = () => {
+        track({ name: 'Privacy Policy Viewed', from: 'disclaimer' })
+        Linking.openURL(config.PRIVACY_POLICY_URL).catch(noop)
+    }
+
+    return (
+        <FFScrollSheet ref={ref} enablePanDownToClose={false} index={0} snapPoints={[600]}>
+            <Box justifyContent="space-between" flex={1} safeAreaBottom>
+                <View>
+                    <Text fontSize="xl" color="white" bold>
+                        {t('title')}
+                    </Text>
+                    <Text color="white" mt="4">
+                        {t('subtitle')}
+                    </Text>
+                    <Text color="white" bold mt="4">
+                        {t('bullet1.title')}
+                    </Text>
+                    <Text color="white" mt="2">
+                        {t('bullet1.text1')}
+                    </Text>
+                    <Text color="white" mt="2">
+                        {t('bullet1.text2')}
+                    </Text>
+                    <Text color="white" mt="4" bold>
+                        {t('bullet2.title')}
+                    </Text>
+                    <Text color="white" mt="2">
+                        {t('bullet2.text')}
+                        <Text style={{ textDecorationLine: 'underline' }} onPress={openPrivacyPolicy}>
+                            {t('bullet2.privacyPolicy')}
+                        </Text>
+                    </Text>
+                    <Text color="white" mt="4">
+                        {t('endText')}
+                    </Text>
+                </View>
+                <FFButton onPress={handleDismiss} bg="blue" borderColor="blue" mt={8}>
+                    <Text fontSize="xl" bold>
+                        {t('confirm')}
+                    </Text>
+                </FFButton>
+            </Box>
+        </FFScrollSheet>
+    )
+})
---
+++ packages/app/src/components/Blocker/index.tsx
@@ -0,0 +1,94 @@
+import { BottomSheetModal } from '@gorhom/bottom-sheet'
+import { pick } from 'lodash'
+import { DateTime, Duration } from 'luxon'
+import { useEffect, useRef, useState } from 'react'
+
+import { usePrivacyPolicyMeta } from '../../api/queries'
+import { useAppStore } from '../../app.store'
+import { track } from '../../tracking'
+import { Disclaimer } from './Disclaimer'
+import { PrivacyPolicyUpdate } from './PrivacyPolicyUpdate'
+
+const DISCLAIMER_INTERVAL = Duration.fromObject({ days: 7 })
+
+const useShouldShowDisclaimer = () => {
+    const [shouldShowDisclaimer, setShouldShowDisclaimer] = useState(true)
+
+    const { dismissedDisclaimerAt } = useAppStore((state) => pick(state, ['dismissedDisclaimerAt', 'appLocked']))
+
+    useEffect(() => {
+        if (dismissedDisclaimerAt !== null) {
+            const now = DateTime.now()
+            const dismissedAtDate = DateTime.fromISO(dismissedDisclaimerAt)
+
+            if (Math.abs(now.diff(dismissedAtDate).as('days')) <= DISCLAIMER_INTERVAL.as('days')) {
+                setShouldShowDisclaimer(false)
+            }
+        }
+    }, [dismissedDisclaimerAt])
+
+    return shouldShowDisclaimer
+}
+
+const useShouldShowPrivacyPolicy = (newestPrivacyPolicyVersion: number | undefined) => {
+    const [shouldShowPrivacyPolicy, setShouldShowPrivacyPolicy] = useState<undefined | boolean>(undefined)
+
+    const acceptedPrivacyPolicy = useAppStore(({ privacyPolicyVersion }) => privacyPolicyVersion)
+
+    useEffect(() => {
+        if (newestPrivacyPolicyVersion === undefined) return
+
+        if (acceptedPrivacyPolicy === null || newestPrivacyPolicyVersion > acceptedPrivacyPolicy) {
+            setShouldShowPrivacyPolicy(true)
+        } else {
+            setShouldShowPrivacyPolicy(false)
+        }
+    }, [acceptedPrivacyPolicy, newestPrivacyPolicyVersion])
+
+    return shouldShowPrivacyPolicy
+}
+
+export const Blocker = () => {
+    const updateStore = useAppStore(({ update }) => update)
+
+    const sheetRef = useRef<BottomSheetModal>(null)
+
+    const newestPrivacyPolicyVersion = usePrivacyPolicyMeta().data?.version
+
+    const shouldShowDisclaimer = useShouldShowDisclaimer()
+    const shouldShowPrivacyPolicy = useShouldShowPrivacyPolicy(newestPrivacyPolicyVersion)
+
+    useEffect(() => {
+        if (shouldShowDisclaimer) {
+            sheetRef.current?.present()
+            track({ name: 'Disclaimer Viewed' })
+        } else if (shouldShowPrivacyPolicy === true) {
+            sheetRef.current?.present()
+            track({ name: 'Privacy Policy Blocker Shown' })
+        } else if (shouldShowPrivacyPolicy === false) {
+            sheetRef.current?.dismiss()
+            updateStore({ appLocked: false })
+        }
+    }, [shouldShowDisclaimer, shouldShowPrivacyPolicy, updateStore])
+
+    const onDismissDisclaimer = () => {
+        sheetRef.current?.dismiss()
+        updateStore({ dismissedDisclaimerAt: DateTime.now().toISO(), privacyPolicyVersion: newestPrivacyPolicyVersion })
+        track({ name: 'Disclaimer Dismissed' })
+    }
+
+    const onDismissPrivacyPolicy = () => {
+        sheetRef.current?.dismiss()
+        updateStore({ privacyPolicyVersion: newestPrivacyPolicyVersion })
+        track({ name: 'Privacy Policy Accepted' })
+    }
+
+    if (shouldShowDisclaimer) {
+        return <Disclaimer onDismiss={onDismissDisclaimer} ref={sheetRef} />
+    }
+    if (shouldShowPrivacyPolicy === true) {
+        return <PrivacyPolicyUpdate onDismiss={onDismissPrivacyPolicy} ref={sheetRef} />
+    }
+
+    return null
+}
