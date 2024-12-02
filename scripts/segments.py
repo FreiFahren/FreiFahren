67b659d8cadd728e431e364b7568badbd69737af
Johan Trieloff <johan@trieloff.net>
2024-12-02 11:30:46 +0100

    fix: avoid duplicate lines
    before we were getting the whole line. This means we were getting both directions and all variations. I have now changed the overpass query to only target a single instance of the line. This will avoid that we have duplicate lines that are stacked on top of each other.

--- scripts/segments.py
+++ scripts/segments.py
@@ -150,22 +150,55 @@ def create_segments(
 
 def main() -> None:
     """
-    Main function to process and generate segments. It will read the overpass export and create segments for all lines.
-    In order to run this script, you need to have the overpass turbo query in the export.geojson file.
-    Run this query to create the data for all lines:
+        Main function to process and generate segments. It will read the overpass export and create segments for all lines.
+        In order to run this script, you need to have the overpass turbo query in the export.geojson file.
+        Run this query to create the data for all lines:
 
-    [out:json][timeout:25];
+        [out:json][timeout:25];
     // Define the area of Berlin
     {{geocodeArea:Berlin}}->.searchArea;
-
-    // Search for specific S-Bahn, U-Bahn, and Metro Tram lines
     (
-      relation["type"="route"]["ref"~"^(S1|S2|S3|S4|S5|S6|S7|S8|S9|S25|S26|S41|S42|S46|S46|S75|S47|S85|U1|U2|U3|U4|U5|U6|U7|U8|U9|M1|M2|M4|M5|M6|M8|M10|M13|M17)$"](area.searchArea);
+      relation(id:1929070); // S1
+      relation(id:2269238); // S2
+      relation(id:2343465); // S3
+      relation(id:2015959); // S5
+      relation(id:2017023); // S7
+      relation(id:2269252); // S8
+      relation(id:2389946); // S9
+      relation(id:2422951); // S25
+      relation(id:7794031); // S26
+      relation(id:14981); // S41
+      relation(id:14983); // S42
+      relation(id:2422929); // S46
+      relation(id:2413846); // S47
+      relation(id:2174798); // S75
+      relation(id:2979451); // S85
+      relation(id:2669205); // U1
+      relation(id:2669184); // U2
+      relation(id:2669208); // U3
+      relation(id:2676945); // U4
+      relation(id:2227744); // U5
+      relation(id:2679164); // U6
+      relation(id:2678986); // U7
+      relation(id:2679014); // U8
+      relation(id:2679017); // U9
+      relation(id:2076230); // M1
+      relation(id:1981932); // M2
+      relation(id:2012424); // M4
+      relation(id:5829220); // M5
+      relation(id:2076355); // M6
+      relation(id:5829222); // M8
+      relation(id:2077032); // M10
+      relation(id:2077077); // M13
+      relation(id:2077162); // M17
+
     );
 
     out body;
     >;
     out skel qt;
+
+    Using the relation id is important to get the line in only one direction.
     """
     # Read the overpass output GeoJSON
     overpass_gdf = gpd.read_file("export.geojson")
