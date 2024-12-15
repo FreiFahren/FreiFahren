31cbbd91693936724436d6aaa89860a3861d9cbc
Johan Trieloff <johan@trieloff.net>
2024-12-15 16:16:36 +0100

    feat: track funnels

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import './index.css'
 import App from './pages/App/App'
@@ -8,11 +8,34 @@ import Impressum from './pages/Impressum/Impressum'
 import reportWebVitals from './reportWebVitals'
 
 import { LocationProvider } from './contexts/LocationContext'
+import { sendAnalyticsEvent } from './utils/analytics'
 
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n'
 import Support from './pages/Support/Support'
 import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy'
+
+type FunnelConfig = {
+    path: string
+    source: string
+}
+
+const FUNNEL_ROUTES: FunnelConfig[] = [
+    {
+        path: '/invite',
+        source: 'FreiFahren_BE Telegram',
+    },
+]
+
+const FunnelRedirect: React.FC<FunnelConfig> = ({ source }) => {
+    sendAnalyticsEvent('Clicked on Funnel link', {
+        meta: {
+            source,
+        },
+    })
+
+    return <Navigate to="/" replace />
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
@@ -32,13 +55,13 @@ root.render(
                     <Route path="/impressum" element={<Impressum />} />
                     <Route path="/datenschutz" element={<PrivacyPolicy />} />
                     <Route path="/support" element={<Support />} />
+                    {FUNNEL_ROUTES.map((config) => (
+                        <Route key={config.path} path={config.path} element={<FunnelRedirect {...config} />} />
+                    ))}
                 </Routes>
             </BrowserRouter>
         </I18nextProvider>
     </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
