8067862ec27f604c46f50ef3e01f3edfb828d62d
Johan Trieloff <johan@trieloff.net>
2024-12-27 10:53:31 +0100

    feat: `FeedbackButton` in `ReportSummaryModal`

--- packages/frontend/src/components/Modals/ReportSummaryModal/ReportSummaryModal.tsx
+++ packages/frontend/src/components/Modals/ReportSummaryModal/ReportSummaryModal.tsx
@@ -1,13 +1,14 @@
 import './ReportSummaryModal.css'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { useCountAnimation } from '../../../hooks/useCountAnimation'
 import { Report } from '../../../utils/types'
 import { ShareButton } from '../../Miscellaneous/ShareButton/ShareButton'
 import { ReportItem } from '../ReportsModal/ReportItem'
-
+import FeedbackButton from 'src/components/Buttons/FeedbackButton/FeedbackButton'
+import { FeedbackForm } from 'src/components/Form/FeedbackForm/FeedbackForm'
 interface ReportSummaryModalProps {
     openAnimationClass?: string
     reportData: Report
@@ -22,13 +23,19 @@ const ReportSummaryModal: React.FC<ReportSummaryModalProps> = ({
     numberOfUsers,
 }) => {
     const { t } = useTranslation()
-    const animatedCount = useCountAnimation(numberOfUsers, 2 * 1000)
+    const animatedCount = useCountAnimation(numberOfUsers, 1.5 * 1000)
+
+    const [showFeedback, setShowFeedback] = useState<boolean>(false)
+    if (showFeedback) {
+        return <FeedbackForm openAnimationClass={openAnimationClass} />
+    }
 
     return (
         <div className={`report-summary-modal container modal ${openAnimationClass}`}>
+            <FeedbackButton onClick={() => setShowFeedback(true)} />
             <div className="report-summary-modal-content">
                 <div>
-                    <img className="no-filter" src={`${process.env.PUBLIC_URL  }/icons/risk-0.svg`} alt="checkmark" />
+                    <img className="no-filter" src={`${process.env.PUBLIC_URL}/icons/risk-0.svg`} alt="checkmark" />
                 </div>
                 <h1>{t('ReportSummaryModal.title')}</h1>
                 <div>
@@ -38,7 +45,7 @@ const ReportSummaryModal: React.FC<ReportSummaryModalProps> = ({
                 <span>
                     <img
                         className="no-filter"
-                        src={`${process.env.PUBLIC_URL  }/icons/users-svgrepo-com.svg`}
+                        src={`${process.env.PUBLIC_URL}/icons/users-svgrepo-com.svg`}
                         alt="users"
                     />
                     <h1>{animatedCount}</h1>
