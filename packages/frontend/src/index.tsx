238450b9ba5e8145348dc581289386b8d639c702
Johan Trieloff <johan@trieloff.net>
2025-01-08 19:44:45 +0100

    feat: only send errors in production

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -13,17 +13,19 @@ import { PrivacyPolicy } from './pages/PrivacyPolicy/PrivacyPolicy'
 import { Support } from './pages/Support/Support'
 import { reportWebVitals } from './reportWebVitals'
 
-Sentry.init({
-    dsn: 'https://555d2661f0b147345e2117aad784560c@o4508609338867712.ingest.de.sentry.io/4508609341161552',
-    integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-    // Tracing
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-})
+if (process.env.NODE_ENV === 'production') {
+    Sentry.init({
+        dsn: 'https://555d2661f0b147345e2117aad784560c@o4508609338867712.ingest.de.sentry.io/4508609341161552',
+        integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+        // Tracing
+        tracesSampleRate: 1.0, //  Capture 100% of the transactions
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+        // Session Replay
+        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    })
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
