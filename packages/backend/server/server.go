57c443c4af6f8f6aae02ff02185ce66b0c5fbed3
Johan Trieloff <johan@trieloff.net>
2025-02-20 16:02:29 +0100

    feat: routing hack
    while it is not really a solution that will scale it will keep the interface and urls clean.

--- packages/backend/server/server.go
+++ packages/backend/server/server.go
@@ -177,6 +177,8 @@ func SetupServer() *echo.Echo {
 
 	v0.POST("/feedback", feedback.PostFeedback)
 
+	v0.GET("/routes", redirectToNavigationService)
+
 	// V1 Routes
 	v1.GET("/risk-prediction/segment-colors", predictionV1.GetRiskSegments)
 
@@ -200,5 +202,12 @@ func SetupServer() *echo.Echo {
 
 	latest.POST("/feedback", feedback.PostFeedback)
 
+	latest.GET("/routes", redirectToNavigationService)
+
 	return e
+}
+
+// this will be replaced with a reverse proxy in the future
+func redirectToNavigationService(c echo.Context) error {
+	return c.Redirect(http.StatusTemporaryRedirect, "http://localhost:7070/routes")
 }
