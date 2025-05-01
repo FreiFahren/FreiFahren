9c1dc48290d46928b4fe6c48078a1c9539e4e852
David Brandes <brandes.1@campus.tu-berlin.de>
2025-05-01 14:07:20 +0200

    Enhance link visibility in telegram messages by converting URLs to clickable hyperlinks in report inspector.

--- packages/nlp_service/services/api_adapter.py
+++ packages/nlp_service/services/api_adapter.py
@@ -29,9 +29,9 @@ def report_inspector() -> tuple:
     if direction:
         telegram_message += f"\n<b>Richtung</b>: {direction}"
     if message:
-        telegram_message += f"\nBeschreibung hier einsehbar: app.freifahren.org/?stationId={stationId}"
+        telegram_message += f"\nBeschreibung hier einsehbar: <a href='app.freifahren.org/?stationId={stationId}'>app.freifahren.org</a>"
     else:
-        telegram_message += f"\n\nMehr Informationen auf app.freifahren.org/?stationId={stationId}"
+        telegram_message += f"\n\nMehr Informationen auf <a href='app.freifahren.org/?stationId={stationId}'>app.freifahren.org</a>"
 
     send_message(FREIFAHREN_CHAT_ID, telegram_message, nlp_bot)
 
