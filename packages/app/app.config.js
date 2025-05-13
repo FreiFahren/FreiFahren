61bf1b27d3a40ecb2ce8601b72b2e7b785c3efc3
Moritz <moritzamando@proton.me>
2025-05-13 13:14:07 +0200

    bump version

--- packages/app/app.config.js
+++ packages/app/app.config.js
@@ -3,7 +3,7 @@ export default {
     expo: {
         name: 'freifahren',
         slug: 'Freifahren',
-        version: '0.1.1',
+        version: '0.1.3',
         orientation: 'portrait',
         icon: './assets/app-icon.png',
         userInterfaceStyle: 'dark',
