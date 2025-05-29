8edd35747433ce45c0ae24a52c051b646fd8cb9b
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2025-05-29 18:01:52 +0200

    fix: set `CenterModal` to be max-h-[80dvh]

--- packages/frontend/src/components/Modal/CenterModal.tsx
+++ packages/frontend/src/components/Modal/CenterModal.tsx
@@ -8,7 +8,7 @@ interface CenterModalProps {
 export const CenterModal = ({ children, className }: CenterModalProps) => {
     return (
         <div
-            className={`bg-background fixed left-1/2 top-1/2 z-10 h-[80dvh] w-[85vw] -translate-x-1/2 -translate-y-1/2 rounded-lg border border-black p-6 text-black shadow-lg ${className}`}
+            className={`bg-background fixed left-1/2 top-1/2 z-10 max-h-[80dvh] w-[85vw] -translate-x-1/2 -translate-y-1/2 rounded-lg border border-black p-6 text-black shadow-lg ${className}`}
         >
             {children}
         </div>
