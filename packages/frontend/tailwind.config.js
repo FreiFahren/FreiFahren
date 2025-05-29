f97a9c4e10a25a42303a98d32170fa706f5b9c34
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2025-05-29 17:59:18 +0200

    chore: install prettier tailwind extension

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                background: '#232323',
+            },
+        },
     },
     plugins: [],
 }
