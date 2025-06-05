443288dbe73a08db9ab71cb7ef6d69a2e94c435a
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2025-06-05 18:07:34 +0200

    feat: Line Selector

--- packages/frontend/src/components/Form/ReportForm/ReportForm2.tsx
+++ packages/frontend/src/components/Form/ReportForm/ReportForm2.tsx
@@ -5,14 +5,33 @@ import FeedbackButton from '../../Buttons/FeedbackButton/FeedbackButton'
 import { FeedbackForm } from '../../Form/FeedbackForm/FeedbackForm'
 import { SelectField } from '../SelectField/SelectField'
 import { Line } from '../../Miscellaneous/Line/Line'
+import { useLines } from 'src/api/queries'
 
 interface ReportFormProps {
     className: string
 }
 
+enum Entity {
+    U = 'U',
+    S = 'S',
+    T = 'T',
+    ALL = '',
+}
+
 export const ReportForm = ({ className }: ReportFormProps) => {
     const [showFeedback, setShowFeedback] = useState<boolean>(false)
-    const [currentEntity, setCurrentEntity] = useState<string>('')
+
+    const [currentEntity, setCurrentEntity] = useState<Entity>(Entity.ALL)
+    const [currentLine, setCurrentLine] = useState<string>('')
+
+    const { data: linesData } = useLines()
+    const allLines = linesData?.map(([line]) => line) ?? []
+    const possibleLines =
+        currentEntity === Entity.ALL
+            ? allLines
+            : currentEntity === Entity.T // exception because T stands for tram but we want Metro trams and regular trams
+              ? allLines.filter((line) => line.startsWith('M') || /^\d+$/.test(line))
+              : allLines.filter((line) => line.startsWith(currentEntity))
 
     if (showFeedback) {
         return <FeedbackForm openAnimationClass={className} />
@@ -28,7 +47,7 @@ export const ReportForm = ({ className }: ReportFormProps) => {
                 <SelectField
                     containerClassName="flex items-center justify-between mx-auto w-full h-10 gap-2"
                     fieldClassName="flex justify-center items-center"
-                    onSelect={(selectedValue) => setCurrentEntity(selectedValue ?? '')}
+                    onSelect={(selectedValue) => setCurrentEntity(selectedValue as Entity)}
                     value={currentEntity}
                 >
                     <button className="flex min-w-0 flex-1 items-center justify-center">
@@ -40,6 +59,20 @@ export const ReportForm = ({ className }: ReportFormProps) => {
                     <button className="flex min-w-0 flex-1 items-center justify-center">
                         <Line line="T" />
                     </button>
+                </SelectField>
+            </section>
+            <section>
+                <h2>Linie</h2>
+                <SelectField
+                    containerClassName="flex items-center justify-between mx-auto w-full overflow-x-visible overflow-y-hidden gap-2"
+                    onSelect={(selectedValue) => setCurrentLine(selectedValue ?? '')}
+                    value={currentLine}
+                >
+                    {possibleLines.map((line) => (
+                        <button key={line} className="flex h-fit min-w-0 flex-1 items-center justify-center">
+                            <Line line={line} />
+                        </button>
+                    ))}
                 </SelectField>
             </section>
         </CenterModal>
