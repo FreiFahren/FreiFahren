e809e0414f0269479c1c2ccbc0120c141a5697ba
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2025-06-26 21:40:48 +0200

    fix: StatsPopUp

--- packages/frontend/src/components/Miscellaneous/StatsPopUp/StatsPopUp.tsx
+++ packages/frontend/src/components/Miscellaneous/StatsPopUp/StatsPopUp.tsx
@@ -1,6 +1,6 @@
 import './StatsPopUp.css'
 
-import React, { useCallback,useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 interface StatsPopUpProps {
@@ -13,7 +13,7 @@ interface StatsPopUpProps {
 const StatsPopUp: React.FC<StatsPopUpProps> = ({ className, numberOfReports, openListModal, numberOfUsers }) => {
     const { t } = useTranslation()
     const [message, setMessage] = useState(
-        `<p><strong>${numberOfReports} ${t('StatsPopUp.reports')}</strong><br /> ${t('StatsPopUp.todayInBerlin')}</p>`
+        `<p class="text-center"><strong>${numberOfReports} ${t('StatsPopUp.reports')}</strong><br /> ${t('StatsPopUp.todayInBerlin')}</p>`
     )
     const [popOut, setPopOut] = useState(false)
     const [isVisible, setIsVisible] = useState(true)
@@ -31,7 +31,7 @@ const StatsPopUp: React.FC<StatsPopUpProps> = ({ className, numberOfReports, ope
     useEffect(() => {
         const updateMessageAndShowPopup = async () => {
             setMessage(
-                `<p>${t('StatsPopUp.over')} <strong> ${numberOfUsers} ${t('StatsPopUp.reporters')}</strong><br /> ${t(
+                `<p class="text-center">${t('StatsPopUp.over')} <strong> ${numberOfUsers} ${t('StatsPopUp.reporters')}</strong><br /> ${t(
                     'StatsPopUp.inBerlin'
                 )}</p>`
             )
@@ -39,11 +39,13 @@ const StatsPopUp: React.FC<StatsPopUpProps> = ({ className, numberOfReports, ope
         }
 
         const timer = setTimeout(() => {
-            updateMessageAndShowPopup().then(hidePopupAfterAnimation).catch(error => {
-                // fix later with sentry
-                // eslint-disable-next-line no-console
-                console.error('Error updating message and showing popup:', error)
-            })
+            updateMessageAndShowPopup()
+                .then(hidePopupAfterAnimation)
+                .catch((error) => {
+                    // fix later with sentry
+                    // eslint-disable-next-line no-console
+                    console.error('Error updating message and showing popup:', error)
+                })
         }, timeForOneMessage)
 
         return () => clearTimeout(timer)
@@ -61,10 +63,7 @@ const StatsPopUp: React.FC<StatsPopUpProps> = ({ className, numberOfReports, ope
     return (
         // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
         <div
-            className={`
-        stats-popup center-child ${className}
-        ${popOut ? 'pop-out' : ''}
-        ${!isVisible ? 'fade-out' : ''}`}
+            className={`stats-popup center-child h-fit ${className} ${popOut ? 'pop-out' : ''} ${!isVisible ? 'fade-out' : ''}`}
             // eslint-disable-next-line react/jsx-handler-names
             onClick={openListModal}
             // eslint-disable-next-line react/no-danger
