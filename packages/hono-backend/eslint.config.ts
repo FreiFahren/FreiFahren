2f0b48a5867d14a9255316de56a71514c571575e
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2025-11-04 16:39:39 +0100

    chore: set up rules

--- packages/hono-backend/eslint.config.ts
+++ packages/hono-backend/eslint.config.ts
@@ -1,4 +1,5 @@
 import eslint from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 
 export default [
@@ -10,7 +11,50 @@ export default [
         project: './tsconfig.json',
       },
     },
+    plugins: {
+        'import': importPlugin,
+    },
     rules: {
+        'import/order': [
+            'warn',
+            {
+                groups: [
+                    'builtin',
+                    'external',
+                    'internal',
+                    'parent',
+                    'sibling',
+                    'index',
+                ],
+                'newlines-between': 'always',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+            },
+        ],
+        'import/no-unresolved': 'warn',
+        'import/no-unused-modules': 'warn',
+        'no-console': 'warn',
+        'no-duplicate-imports': 'warn',
+        'no-use-before-define': 'warn',
+        'no-useless-assignment': 'warn',
+        'capitalized-comments': 'warn',
+        'no-bitwise': 'warn',
+        eqeqeq: 'error',
+        'no-empty-function': 'error',
+        'no-eq-null': 'error',
+        'no-lonely-if': 'warn',
+        'no-nested-ternary': 'warn',
+        'no-redeclare': 'error',
+        'no-useless-catch': 'error',
+        'no-var': 'warn',
+        'prefer-const': 'warn',
+        'max-depth': ['error', 4],
+        complexity: ['error'],
+
+
+
     },
   },
   {
