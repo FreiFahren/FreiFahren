9873c0ba44013b6e1634558ec7eb26bb4cd27ed4
Moritz <moritzamando@proton.me>
2025-11-12 21:18:41 +0100

    Remove postprocessing

--- packages/hono-backend/src/modules/reports/reports-service.ts
+++ packages/hono-backend/src/modules/reports/reports-service.ts
@@ -1,5 +1,4 @@
 import { and, gte, lte } from 'drizzle-orm'
-import { isNil } from 'lodash'
 import { DateTime } from 'luxon'
 
 import { DbConnection, InsertReport, reports } from '../../db/'
@@ -28,58 +27,6 @@ export class ReportsService {
     }
 
     async createReport(reportData: InsertReport) {
-        const postprocessed = await this.postprocessReport(reportData)
-
-        const result = await this.db.insert(reports).values(postprocessed)
-
-        return result
-    }
-
-    private async postprocessReport(reportData: InsertReport) {
-        return [this.postprocessLineId.bind(this), this.postprocessDirectionId.bind(this)].reduce(
-            (acc, curr) => acc.then((val) => curr(val)),
-            Promise.resolve(reportData)
-        )
-    }
-
-    private async postprocessLineId(reportData: InsertReport): Promise<InsertReport> {
-        if (isNil(reportData.lineId)) {
-            const linesWithStation = await this.stationsService.getLinesForStation(reportData.stationId)
-
-            return {
-                ...reportData,
-                lineId: linesWithStation.length === 1 ? linesWithStation[0].id : null,
-            }
-        }
-
-        return {
-            ...reportData,
-            lineId: (await this.linesService.isStationOnLine(reportData.stationId, reportData.lineId))
-                ? reportData.lineId
-                : null,
-        }
-    }
-
-    private async postprocessDirectionId(reportData: InsertReport): Promise<InsertReport> {
-        if (isNil(reportData.lineId) || isNil(reportData.directionId)) {
-            return {
-                ...reportData,
-                directionId: null,
-            }
-        }
-
-        if (await this.linesService.isLineCircular(reportData.lineId)) {
-            return {
-                ...reportData,
-                directionId: null,
-            }
-        }
-
-        return {
-            ...reportData,
-            directionId: (await this.linesService.isValidDirectionForLine(reportData.lineId, reportData.directionId))
-                ? reportData.directionId
-                : null,
-        }
+        return this.db.insert(reports).values(reportData)
     }
 }
