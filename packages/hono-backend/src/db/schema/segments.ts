8ffae16b194f31cbde0e9a71f005d56e1a81be41
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2026-02-18 18:07:08 +0100

    fix: unique constraint
    Each line with from and to can only exist once

--- packages/hono-backend/src/db/schema/segments.ts
+++ packages/hono-backend/src/db/schema/segments.ts
@@ -1,19 +1,23 @@
-import { jsonb, pgTable, serial, varchar } from 'drizzle-orm/pg-core'
+import { jsonb, pgTable, serial, uniqueIndex, varchar } from 'drizzle-orm/pg-core'
 
 import { lines } from './lines'
 import { stations } from './stations'
 
-export const segments = pgTable('segments', {
-    id: serial().primaryKey(),
-    lineId: varchar({ length: 16 })
-        .notNull()
-        .references(() => lines.id, { onDelete: 'cascade' }),
-    fromStationId: varchar({ length: 16 })
-        .notNull()
-        .references(() => stations.id, { onDelete: 'cascade' }),
-    toStationId: varchar({ length: 16 })
-        .notNull()
-        .references(() => stations.id, { onDelete: 'cascade' }),
-    color: varchar({ length: 7 }).notNull(),
-    coordinates: jsonb().notNull().$type<[number, number][]>(),
-})
+export const segments = pgTable(
+    'segments',
+    {
+        id: serial().primaryKey(),
+        lineId: varchar({ length: 16 })
+            .notNull()
+            .references(() => lines.id, { onDelete: 'cascade' }),
+        fromStationId: varchar({ length: 16 })
+            .notNull()
+            .references(() => stations.id, { onDelete: 'cascade' }),
+        toStationId: varchar({ length: 16 })
+            .notNull()
+            .references(() => stations.id, { onDelete: 'cascade' }),
+        color: varchar({ length: 7 }).notNull(),
+        coordinates: jsonb().notNull().$type<[number, number][]>(),
+    },
+    (table) => [uniqueIndex('segments_line_from_to_idx').on(table.lineId, table.fromStationId, table.toStationId)]
+)
