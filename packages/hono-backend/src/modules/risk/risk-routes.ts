5ad6253d3862172e0f65123dda6228c9b6cba150
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2026-04-16 13:56:09 +0200

    fix: use versioned routes

--- packages/hono-backend/src/modules/risk/risk-routes.ts
+++ packages/hono-backend/src/modules/risk/risk-routes.ts
@@ -3,7 +3,7 @@ import { defineRoute } from '../../common/router'
 
 export const getRisk = defineRoute<Env>()({
     method: 'get',
-    path: 'v0/risk',
+    path: '/',
     handler: async (c) => {
         const riskService = c.get('riskService')
         return c.json(await riskService.getRisk())
