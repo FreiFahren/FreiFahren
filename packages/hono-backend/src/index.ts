ad1e220f2d0f8b85b1460e77dccd4598fd1f60c5
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2026-04-23 17:27:23 +0200

    refactor: extract logger

--- packages/hono-backend/src/index.ts
+++ packages/hono-backend/src/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from 'hono'
 import { requestId } from 'hono/request-id'
 import { pinoLogger } from 'hono-pino'
-import pino from 'pino'
 
 import { registerServices, Services, type Env } from './app-env'
 import { handleError } from './common/error-handler'
+import { logger } from './common/logger'
 import { registerVersionedRoutes } from './common/router'
 import { db, DbConnection } from './db'
 import { postFeedback } from './modules/feedback/feedback-routes'
@@ -30,30 +30,7 @@ export const createApp = (dbConnection: DbConnection = db) => {
     app.use(requestId())
     app.use(
         pinoLogger({
-            pino: pino({
-                level: process.env.LOG_LEVEL ?? 'info',
-                transport: {
-                    targets: [
-                        {
-                            target: 'pino-pretty',
-                            options: {
-                                colorize: true,
-                                ignore: 'pid,hostname,req,res,responseTime,reqId',
-                                translateTime: 'SYS:standard',
-                                destination: 1,
-                            },
-                        },
-                        {
-                            target: 'pino-roll',
-                            options: {
-                                file: './app.log',
-                                frequency: 'daily',
-                                mkdir: true,
-                            },
-                        },
-                    ],
-                },
-            }),
+            pino: logger,
         })
     )
 
