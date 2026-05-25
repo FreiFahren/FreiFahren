e7bd6a00cb7d2da28785dc0abcedb5bb09b10e89
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2026-05-25 13:51:57 +0200

    chore: drop tanstack dev tools

--- packages/frontend-next/src/routes/__root.tsx
+++ packages/frontend-next/src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const Route = createRootRoute({
@@ -10,7 +9,6 @@ function RootComponent() {
   return (
     <>
       <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
       <ReactQueryDevtools buttonPosition="bottom-left" />
     </>
   );
