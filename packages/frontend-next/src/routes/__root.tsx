0701082a377d4bc8e89d3fe8ef8d44057ee60cf3
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2026-05-25 14:01:07 +0200

    chore: drop tanstack query dev tools

--- packages/frontend-next/src/routes/__root.tsx
+++ packages/frontend-next/src/routes/__root.tsx
@@ -1,15 +1,9 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
-  return (
-    <>
-      <Outlet />
-      <ReactQueryDevtools buttonPosition="bottom-left" />
-    </>
-  );
+  return <Outlet />;
 }
