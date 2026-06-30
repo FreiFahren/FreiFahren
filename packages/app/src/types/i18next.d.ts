01a07b5bb512e98463011db0785492e852a5a059
Johan Trieloff <30388999+johan-t@users.noreply.github.com>
2026-06-30 12:23:00 +0200

    chore: remove the legacy React Native app package and its workflow

--- packages/app/src/types/i18next.d.ts
+++
@@ -1,8 +0,0 @@
-import de from '../assets/locales/de.json'
-
-declare module 'i18next' {
-    interface CustomTypeOptions {
-        defaultNS: 'common'
-        resources: typeof de
-    }
-}
--- packages/app/src/utils.ts
+++
@@ -1,1 +0,0 @@
-export const filterNullish = <T>(value: T | null | undefined): value is T => value !== null && value !== undefined
